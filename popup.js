e48385731f1cdcf5dde933ff4a178833fb7d7d4a
Eliot Cholerton-Hill <eliot.cholerton-hill@localhost.localdomain>
2019-07-07 16:33:58 +0100

    Added "Unstackstaging" button

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
-function modifyUrlString(url){
+function modifyUrlStringToStack(url){
 	if (!url.includes("stackstaging")){
 		var protocolRegex = /^(http|https):\/\//;
 		var wwwRegex = /www\./;
 		var dotRegex = /\./;
 
-		url = url.replace(protocolRegex, "")
-		url = url.replace(wwwRegex, "")
+		url = url.replace(protocolRegex, "");
+		url = url.replace(wwwRegex, "");
 		
 		slashIndex = url.indexOf("/");
-		query = url.substring(slashIndex != -1 ? slashIndex : url.length, url.length)
+		query = url.substring(slashIndex != -1 ? slashIndex : url.length, url.length);
 
-		url = url.substring(0, slashIndex != -1 ? slashIndex : url.length)
+		url = url.substring(0, slashIndex != -1 ? slashIndex : url.length);
 
 		url = url.replace(dotRegex, "-");
 		
@@ -20,25 +20,78 @@ function modifyUrlString(url){
 	else {return -1;}
 }
 
-function setUrl(event) {
+function modifyUrlStringFromStack(url){
+	if (url.includes("stackstaging")){
+		var protocolRegex = /^(http|https):\/\//;
+		var dashRegex = /\-/;
+
+		url = url.replace(protocolRegex, "");
+		
+		slashIndex = url.indexOf("/");
+		dotIndex = url.indexOf(".");
+		query = url.substring(slashIndex != -1 ? slashIndex : url.length, url.length);
+
+		url = url.substring(0, dotIndex != -1 ? dotIndex : url.length);
+
+		url = url.replace(dashRegex, ".");
+		
+		url = "https://" + url + query;
+		return url;
+	}
+	else {return -1;}
+}
+
+function setUrl(url){
 	chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
 	function(tab){
-		url = tab[0].url
-		
-		modified_url = modifyUrlString(url)
-		if(modified_url!=-1){
 		chrome.tabs.update(tab.id, {url: modified_url});
+		});
+}
+
+function setStackUrl(event) {
+	removeMessage();
+	chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+	function(tab){
+		url = tab[0].url;
+		modified_url = modifyUrlStringToStack(url);
+		if(modified_url!=-1){
+		setUrl(modified_url)
 		}
 		else {
-			if(document.getElementById("main-popup").contains(document.getElementById("warning"))){
-				removed = document.getElementById("main-popup").removeChild(document.getElementById("warning"));
-			}
-			warning = document.createElement("p");
-			warning.innerHTML = "Already StackStaging!"
-			warning.id = "warning";
-			document.getElementById('main-popup').appendChild(warning);
+			addMessage("Already StackStaging!");
 		}
 	});
 }
-document.getElementById('stackstaging').addEventListener('click', setUrl);
+
+function unsetStackUrl(event) {
+	removeMessage();
+	chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+	function(tab){
+		url = tab[0].url;
+		modified_url = modifyUrlStringFromStack(url);
+		if(modified_url!=-1){
+		setUrl(modified_url)
+		}
+		else {
+			addMessage("Not on Stackstaging!");
+			
+		}
+	});
+}
+
+function addMessage(text){
+	message = document.createElement("p");
+	message.innerHTML = text;
+	message.id = "message";
+	document.getElementById('main-popup').appendChild(message);
+}
+
+function removeMessage(){
+	if(document.getElementById("main-popup").contains(document.getElementById("message"))){
+		removed=document.getElementById("main-popup").removeChild(document.getElementById("message"));
+	}
+}
+
+document.getElementById('stackstaging-button').addEventListener('click', setStackUrl);
+document.getElementById('unstackstaging-button').addEventListener('click', unsetStackUrl);
 
